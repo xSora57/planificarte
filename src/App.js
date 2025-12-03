6cceb3431f30d9376c517ed7f510319d36087e60
xSora57 <ernesto.marmar@educa.jcyl.es>
2025-12-03 13:28:19 +0100

    nube8

--- src/App.js
+++ src/App.js
@@ -25,14 +25,6 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
-const urlParams = new URLSearchParams(window.location.search);
-const tokenFromUrl = urlParams.get("token");
-if (tokenFromUrl) {
-  localStorage.setItem("token", tokenFromUrl);
-  setToken(tokenFromUrl);
-  setIsLoggedIn(true);
-  window.history.replaceState({}, "", window.location.pathname);
-}
 /* APP */
 function App() {
   /*  Estados  */
